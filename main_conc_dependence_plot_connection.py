175f0a7110e11c435a79047efa2cc0a8becb0094
urakubo <hurakubo@gmail.com>
2024-03-16 14:17:54 +0900

    WIP

--- main_conc_dependence_plot_connection.py
+++ main_conc_dependence_plot_connection.py
@@ -17,7 +17,7 @@ plt.rcParams.update( {'font.size': 5} )
 
 
 
-def plot_a_binding(ax, multi_graph, prefix, species, distribution_or_average = 'distribution'):
+def get_statistics_connection(multi_graph, species, distribution_or_average):
 	
 	
 	ids = [ i for i, attr in multi_graph.nodes('species') if attr == species ]
@@ -28,14 +28,29 @@ def plot_a_binding(ax, multi_graph, prefix, species, distribution_or_average = '
 		ymax = 1000
 		#nx.get_node_attributes(multi_graph, 'species').keys()
 	elif species == 'CaMKII' and distribution_or_average == 'average':
-		numbers_of_connections = [ multi_graph.degree[id] for id in ids ]
-		dist = {'GluN2B \n {:.2f}'.format(ave_GluN2B): np.average(numbers_of_connections)}
+		numbers_of_connections = np.average( [ multi_graph.degree[id] for id in ids ] )
+		dist = {'GluN2B \n {:.2f}'.format(numbers_of_connections): numbers_of_connections}
 		ymax = 12
-	if species == 'STG' and distribution_or_average == 'distribution':
+	elif species == 'STG' and distribution_or_average == 'distribution':
 		numbers_of_connections = [ multi_graph.degree[id] for id in ids ]
 		dist = {'{:d} PSD95'.format(i): numbers_of_connections.count(i) for i in range(5)}
 		ymax = 4000
 		#nx.get_node_attributes(multi_graph, 'species').keys()
+	elif species == 'STG' and distribution_or_average == 'average':
+		numbers_of_connections = np.average( [ multi_graph.degree[id] for id in ids ] )
+		dist = {'PSD95 \n {:.2f}'.format(numbers_of_connections): numbers_of_connections}
+		ymax = 4
+		
+	elif species == 'GluN2B' and distribution_or_average == 'average':
+		ymax = 4
+		edges_from_molecules = [ multi_graph.edges(id, keys=True, data=True) for id in ids ]
+		connections_from_one_molecules = [ [e[3]['type_connection'] for e in es] for es in edges_from_molecules ]
+		
+		num_GluN2B_CaMKII = np.average( [ c.count('GluN2B_CaMKII') for c in connections_from_one_molecules ] )
+		num_GluN2B_PSD95  = np.average( [ c.count('GluN2B_PSD95') for c in connections_from_one_molecules ] )
+		dist = {'GluN2B_CaMKII \n {:.2f}'.format(num_GluN2B_CaMKII): num_GluN2B_CaMKII,
+				'GluN2B_PSD95  \n {:.2f}'.format(num_GluN2B_PSD95) : num_GluN2B_PSD95 }
+		
 	elif species == 'GluN2B' and distribution_or_average == 'distribution':
 		ymax = 8000
 		reference_types_connection = [\
@@ -59,6 +74,17 @@ def plot_a_binding(ax, multi_graph, prefix, species, distribution_or_average = '
 			for j, ref in enumerate( reference_types_connection ):
 				if utils.equal_list(connections_from_a_molecule, ref):
 					dist[titles[j]] += 1
+
+	elif species == 'PSD95' and distribution_or_average == 'average':
+		ymax = 4
+		edges_from_molecules = [ multi_graph.edges(id, keys=True, data=True) for id in ids ]
+		connections_from_one_molecules = [ [e[3]['type_connection'] for e in es] for es in edges_from_molecules ]
+		
+		num_STG_PSD95    = np.average( [c.count('STG_PSD95')  for c in connections_from_one_molecules ]   )
+		num_GluN2B_PSD95 = np.average( [c.count('GluN2B_PSD95') for c in connections_from_one_molecules ] )
+		dist = {'STG_PSD95 \n {:.2f}'.format(num_STG_PSD95): num_STG_PSD95, \
+				'GluN2B_PSD95 \n {:.2f}'.format(num_GluN2B_PSD95): num_GluN2B_PSD95 }
+
 	elif species == 'PSD95' and distribution_or_average == 'distribution':
 		ymax = 3000
 		reference_types_connection = [\
@@ -81,56 +107,17 @@ def plot_a_binding(ax, multi_graph, prefix, species, distribution_or_average = '
 			for j, ref in enumerate( reference_types_connection ):
 				if utils.equal_list(connections_from_a_molecule, ref):
 					dist[titles[j]] += 1
+	
+	return dist
 
-	elif species == 'PSD95' and distribution_or_average == 'average':
-
-		numbers_of_connections = [ multi_graph.degree[id] for id in ids ]
-		dist = {'GluN2B \n {:.2f}'.format(ave_GluN2B): np.average(numbers_of_connections)}
-		ymax = 12
-
-		edges_from_molecules = [ multi_graph.edges(id, keys=True, data=True) for id in ids ]
-		connections_from_one_molecules = [ [e[3]['type_connection'] for e in es] for es in edges_from_molecules ]
-		
-		
-		ymax = 3000
-		reference_types_connection = [\
-			[], \
-			['STG_PSD95'],['STG_PSD95']*2,['STG_PSD95']*3, \
-			['GluN2B_PSD95'], ['GluN2B_PSD95']*2, ['GluN2B_PSD95']*3,\
-			['STG_PSD95', 'GluN2B_PSD95'],\
-			['STG_PSD95']*2 + ['GluN2B_PSD95'],\
-			['STG_PSD95'] + ['GluN2B_PSD95']*2]
-		titles = [\
-			'None', \
-			'1 STG', '2 STG', '3 STG', \
-			'1 GluN2B', '2 GluN2B', '3 GluN2B', \
-			'1 STG, 1 GluN2B', '2 STG, 1 GluN2B', '1 STG, 2 GluN2B'
-			 ]
-		dist = {t: 0 for t in titles}
-		edges_from_molecules = [ multi_graph.edges(id, keys=True, data=True) for id in ids ]
-		connections_from_one_molecules = [ [e[3]['type_connection'] for e in es] for es in edges_from_molecules ]
-		for connections_from_a_molecule in connections_from_one_molecules:
-			for j, ref in enumerate( reference_types_connection ):
-				if utils.equal_list(connections_from_a_molecule, ref):
-					dist[titles[j]] += 1
-
-
-	ax.set_title(prefix)
-	ax.bar(*zip(*dist.items()), width=0.5)
-	ax.spines['right'].set_visible(False)
-	ax.spines['top'].set_visible(False)
-	ax.set_ylim(0,ymax)
-	ax.set_ylabel('(Number)')
-	# ax.set_aspect(1.0 / ax.get_data_ratio())
-
-	if distribution_or_average == 'distribution':
-		ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
 
 if __name__ == '__main__':
 
 	
 	# Dataset 1
-	species = 'STG' # 'STG','GluN2B', 'PSD95','CaMKII', 'PSD95_connection'
+	species = 'PSD95' # 'STG','GluN2B', 'PSD95','CaMKII', 'PSD95_connection'
+	distribution_or_average = 'average'
+	
 	dir_target  = 'conc_dependence'
 	dir_target2 = 'nums_connection'
 	
@@ -153,6 +140,7 @@ if __name__ == '__main__':
 	
 	vals = np.zeros([num_rows, num_columns])
 	fig  = plt.figure(figsize=(10, 10), tight_layout=True)
+	plt.suptitle('Species: {}, {}'.format( species, distribution_or_average ), )
 	#
 	for i, stg in enumerate(STG):
 		for j, glun in enumerate(GluN2B):
@@ -164,12 +152,25 @@ if __name__ == '__main__':
 			graph = g[0]
 			multi_graph = g[1]
 			#sys.exit(0)
+			
+			dist = get_statistics_connection(multi_graph, species, distribution_or_average)
+			
 			title = prefix # prefix, None
 			row    = num_rows-j-1
 			column = i+1
 			print('column: ', column, ', row: ', row)
 			ax = fig.add_subplot( num_rows, num_columns, row*num_columns+column )
-			plot_a_binding(ax, multi_graph, prefix, species)
+			ax.set_title(prefix)
+			ax.bar(*zip(*dist.items()), width=0.5)
+			ax.spines['right'].set_visible(False)
+			ax.spines['top'].set_visible(False)
+			ax.set_ylim(0,ymax)
+			ax.set_ylabel('(Number)')
+			# ax.set_aspect(1.0 / ax.get_data_ratio())
+
+			if distribution_or_average == 'distribution':
+				ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+
 	
 	# Save figure
 	filename = 'ave_num_binding_{}'.format(species)
--- obsoletes/main_all_edit_connection_graph.py
+++ obsoletes/main_all_edit_connection_graph.py
@@ -19,92 +19,62 @@ def get_clusters(ids_molecule, types, bp):
 	
 	unique_ids_molecule = np.unique( ids_molecule )
 	graph = nx.Graph()
-	multi_graph = nx.MultiGraph()
 	CaMKII_or_GluN2B = np.zeros( len(ids_molecule), dtype = 'int')
 	
 	# Create nodes
 	for id in unique_ids_molecule:
 		flags = (id == ids_molecule)
-		ids_bead         = np.nonzero(flags)
-		types_bead       = types[flags]
+		ids_bead = np.nonzero(flags)
+		types_bead = types[flags]
 		ids_partner_bead = bp[flags]
 		species = [k for k, i in p.molecules_without_all.items() if types_bead[0] in i['id']][0]
 		
-		graph.add_node(id, \
-			species    = species, \
-			ids_bead   = ids_bead, \
-			types_bead = types_bead, \
-			num_beads  = np.sum(flags), \
-			ids_partner_bead =ids_partner_bead)
-		
-		multi_graph.add_node(id,\
-			species    = species, \
-			ids_bead   = ids_bead, \
-			types_bead = types_bead, \
-			num_beads  = np.sum(flags), \
-			ids_partner_bead =ids_partner_bead)
-		
+		if species in ['CaMKII','GluN2B']:
+			CaMKII_or_GluN2B[flags] = 1
+			graph.add_node(id, species = species, \
+				ids_bead   = ids_bead, \
+				types_bead = types_bead, \
+				num_beads  = np.sum(flags), \
+				ids_partner_bead =ids_partner_bead)
+	
 	# Make connection
 	list_ids = list(graph.nodes.keys())
-	ids_bead_already_connected = np.zeros_like( bp, dtype='int' )
 	for i in range(ids_molecule.shape[0]):
-		if 	(bp[i] >= 0) and \
-			(ids_bead_already_connected[i] == 0) and \
-			(ids_bead_already_connected[bp[i]] == 0):
-			
-			ids_bead_already_connected[i]     = 1
-			ids_bead_already_connected[bp[i]] = 1
-			
+		if (bp[i] >= 0) and (CaMKII_or_GluN2B[bp[i]] == 1) and (CaMKII_or_GluN2B[i] == 1):
 			id_molecule = ids_molecule[i]
 			id_molecule_partner = ids_molecule[bp[i]]
-			
-			species = graph.nodes[id_molecule]['species']
-			species_partner    = graph.nodes[id_molecule_partner]['species']
-			connecting_species = [species, species_partner]
-			if ('GluN2B' in connecting_species) and ('CaMKII' in connecting_species):
-				type_connection = 'GluN2B_CaMKII'
-			elif ('GluN2B' in connecting_species) and ('PSD95' in connecting_species):
-				type_connection = 'GluN2B_PSD95'
-			elif ('STG' in connecting_species) and ('PSD95' in connecting_species):
-				type_connection = 'STG_PSD95'
-			else:
-				raise ValueError("Erronous connection: {}", connecting_species)
-			
-			graph.add_edge(id_molecule, id_molecule_partner, type_connection = type_connection)
-			multi_graph.add_edge(id_molecule, id_molecule_partner, type_connection = type_connection)
-			
-	
-	
-	
-	return graph, multi_graph
+			graph.add_edge(id_molecule, id_molecule_partner)
+	return graph
 	
 	
 if __name__ == '__main__':
 	
 	
+	
 	# Valency length
-	#'''
+	'''
 	subdirs    = ['val_{}'.format(i) for i in range(2,14,2)]
 	filenames  = ['R2_{}.lammpstrj'.format(str(i).zfill(3)) for i in range(7)]
 	filenames_input  = [ os.path.join(d, f) for d in subdirs for f in filenames]
 	filenames_output = [ str(id_d).zfill(2)+'_'+str(id_f).zfill(3) for id_d in range(2,14,2) for id_f in range(7) ]
 	dir_input        = 'valency_length'
 	dir_edited_data  = 'valency_length'
-	#'''
+	'''
 	
 	# Conc dependnece
-	'''
+	#'''
 	filenames_output = [str(i).zfill(3) for i in range(48) ]
 	filenames_input  = ['R2_{}.lammpstrj'.format(f) for f in filenames_output ] #70
 	dir_input        = 'conc_dependence'
 	dir_edited_data  = 'conc_dependence'
-	'''
+	#'''
+	
 	
 	# Shared part of initialization
 	dir_lammpstrj    = os.path.join('..', 'lammpstrj3', dir_input)
 	dir_edited_data  = os.path.join('data3',dir_edited_data)
 	os.makedirs(dir_edited_data, exist_ok=True)
-	suffix = 'graph2'
+	suffix = 'graph'
 	
 	#
 	for filename_input, filename_output in zip(filenames_input, filenames_output):
@@ -122,10 +92,10 @@ if __name__ == '__main__':
 		
 		
 		# Generate graph
-		g = get_clusters(ids_molecule, types, bp)
+		graph = get_clusters(ids_molecule, types, bp)
 		
 		
 		# Save the edited data
 		prefix = filename_output
-		utils.save(dir_edited_data, prefix, suffix, g)
+		utils.save(dir_edited_data, prefix, suffix, graph)
 		
